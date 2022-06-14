40d86d7ae895e77ad4c6b3044db8222a49b201e0
Mundanity Fan <i@mundanity.moe>
2022-06-14 19:49:36 +0800

    添加注释

--- ImgProcess.py
+++ ImgProcess.py
@@ -5,6 +5,9 @@ import random
 
 
 class ImgProcess:
+    """
+    图片加载与处理类
+    """""
     def __init__(self, path):
         self.path = path
         self.data_list = []
@@ -16,6 +19,10 @@ class ImgProcess:
         self.y_test = []
 
     def load_img_data(self):
+        """
+        加载指定目录中的图片，并根据文件名创建标签，并对图片进行灰度化处理，结果作为数据集
+        :return: 无返回值
+        """""
         images = os.listdir(self.path)
         data_list = []
         label_list = []
@@ -36,6 +43,10 @@ class ImgProcess:
         self.label_list = label_list
 
     def get_split_index(self):
+        """
+        对每个人随机选取 7 张照片作为训练集
+        :return: 无返回值
+        """""
         split_list = []
         while len(split_list) < 7:
             x = random.randint(0, 25)
@@ -48,6 +59,10 @@ class ImgProcess:
         self.split_list = split_list
 
     def dataset_split(self):
+        """
+        进行数据集的分割
+        :return: 无返回值
+        """""
         x_test = []
         y_test = []
         for x in reversed(self.split_list):
@@ -59,6 +74,10 @@ class ImgProcess:
         self.y_test = y_test
 
     def run(self):
+        """
+        进行图片加载分割操作
+        :return: 训练数据与标签集，测试数据与标签集
+        """""
         self.load_img_data()
         self.get_split_index()
         self.dataset_split()
