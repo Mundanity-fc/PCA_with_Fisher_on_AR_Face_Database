a153fd83e93baf8959a7a9a31569c1cce1745469
Mundanity Fan <i@mundanity.moe>
2022-06-14 19:21:04 +0800

    整合图像处理和数据处理函数为 ImgProcess 类

---
+++ ImgProcess.py
@@ -0,0 +1,66 @@
+from PIL import Image
+import os
+import numpy
+import random
+
+
+class ImgProcess:
+    def __init__(self, path):
+        self.path = path
+        self.data_list = []
+        self.label_list = []
+        self.split_list = []
+        self.x_train = []
+        self.x_test = []
+        self.y_train = []
+        self.y_test = []
+
+    def load_img_data(self):
+        images = os.listdir(self.path)
+        data_list = []
+        label_list = []
+        for x in images:
+            if not (x.startswith('M-')) and not (x.startswith('W-')):
+                continue
+            sex, pid, sp = x.split('-')
+            img = Image.open(self.path + x)
+            img = img.convert('L')
+            data = numpy.array(img, 'f')
+            data = data.ravel()
+            data_list.append(data)
+            if sex == 'M':
+                label_list.append(pid)
+            else:
+                label_list.append(str(int(pid) + 50))
+        self.data_list = data_list
+        self.label_list = label_list
+
+    def get_split_index(self):
+        split_list = []
+        while len(split_list) < 7:
+            x = random.randint(0, 25)
+            if split_list.count(x) == 0:
+                split_list.append(x)
+        split_list.sort()
+        for x in range(2, 101):
+            for i in range(0, 7):
+                split_list.append(split_list[i] + (x - 1) * 26)
+        self.split_list = split_list
+
+    def dataset_split(self):
+        x_test = []
+        y_test = []
+        for x in reversed(self.split_list):
+            x_test.append(self.data_list.pop(x))
+            y_test.append(self.label_list.pop(x))
+        self.x_train = numpy.array(self.data_list) / 255
+        self.y_train = self.label_list
+        self.x_test = numpy.array(x_test) / 255
+        self.y_test = y_test
+
+    def run(self):
+        self.load_img_data()
+        self.get_split_index()
+        self.dataset_split()
+        return self.x_train, self.y_train, self.x_test, self.y_test
+
