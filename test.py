04f77711ffb7a3643b50dafda678493ce4554a31
Mundanity Fan <i@mundanity.moe>
2022-06-14 19:21:26 +0800

    进行多任务的测试

--- test.py
+++ test.py
@@ -1,71 +1,64 @@
-import os
-import random
-
-import numpy
-from PIL import Image
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+from customPCA import customPCA
+from ImgProcess import ImgProcess
 from sklearn.decomposition import PCA
 from Fisher import Fisher
 
-def load_img_data(path):
 
-    images = os.listdir(path)
-    data_list = []
-    label_list = []
-    for x in images:
-        if not (x.startswith('M-')) and not (x.startswith('W-')):
-            continue
-        sex, pid, sp = x.split('-')
-        img = Image.open(path + x)
-        img = img.convert('L')
-        data = numpy.array(img, 'f')
-        data = data.ravel()
-        data_list.append(data)
-        if sex == 'M':
-            label_list.append(pid)
-        else:
-            label_list.append(str(int(pid) + 50))
-    return data_list, label_list
-
-
-def get_split_index():
-    split_list = []
-    while len(split_list) < 7:
-        x = random.randint(0, 25)
-        if split_list.count(x) == 0:
-            split_list.append(x)
-    split_list.sort()
-    for x in range(2, 101):
-        for i in range(0, 7):
-            split_list.append(split_list[i] + (x - 1) * 26)
-    return split_list
-
-
-def dataset_split(data_list: list, label_list: list, split_list: list):
-    x_test = []
-    y_test = []
-    for x in reversed(split_list):
-        x_test.append(data_list.pop(x))
-        y_test.append(label_list.pop(x))
-    x_train = data_list
-    y_train = label_list
-    x_train = numpy.array(x_train) / 255
-    x_test = numpy.array(x_test) / 255
-    return x_train, y_train, x_test, y_test
-
-
-if __name__ == '__main__':
-    data_list, label_list = load_img_data('./dataset/')
-    split_list = get_split_index()
-    x_train, y_train, x_test, y_test = dataset_split(data_list, label_list, split_list)
-    Fisher = Fisher()
-
+def task1():
+    imgProcess = ImgProcess('./dataset/')
+    x_train, y_train, x_test, y_test = imgProcess.run()
     pca = PCA(n_components=50, whiten=True, random_state=0).fit(x_train)
     x_train_pca = pca.transform(x_train)
     x_test_pca = pca.transform(x_test)
-    Fisher.calculate(x_train_pca, y_train)
-    # knn = KNeighborsClassifier(n_neighbors=10)
-    # knn.fit(x_train_pca, y_train)
-    # prediction = knn.predict(x_test_pca)
-    # print(accuracy_score(y_test, prediction))
+    knn = KNeighborsClassifier(n_neighbors=10)
+    knn.fit(x_train_pca, y_train)
+    prediction = knn.predict(x_test_pca)
+    print(accuracy_score(y_test, prediction))
+
+
+def task2():
+    imgProcess = ImgProcess('./dataset/')
+    x_train, y_train, x_test, y_test = imgProcess.run()
+    pca1 = PCA(n_components=50, whiten=True, random_state=0).fit(x_train)
+    x_train_pca1 = pca1.transform(x_train)
+    x_test_pca1 = pca1.transform(x_test)
+    pca2 = PCA(n_components=500, whiten=True, random_state=0).fit(x_train)
+    x_train_pca2 = pca1.transform(x_train)
+    x_test_pca2 = pca1.transform(x_test)
+    pca3 = PCA(n_components=5000, whiten=True, random_state=0).fit(x_train)
+    x_train_pca3 = pca1.transform(x_train)
+    x_test_pca3 = pca1.transform(x_test)
+    pca4 = PCA(n_components=10000, whiten=True, random_state=0).fit(x_train)
+    x_train_pca4 = pca1.transform(x_train)
+    x_test_pca4 = pca1.transform(x_test)
+    knn = KNeighborsClassifier(n_neighbors=10)
+    knn.fit(x_train_pca1, y_train)
+    prediction = knn.predict(x_test_pca1)
+    print('保留50个特征值时准确率为：%.5f' % accuracy_score(y_test, prediction))
+    knn.fit(x_train_pca2, y_train)
+    prediction = knn.predict(x_test_pca2)
+    print('保留500个特征值时准确率为：%.5f' % accuracy_score(y_test, prediction))
+    knn.fit(x_train_pca3, y_train)
+    prediction = knn.predict(x_test_pca3)
+    print('保留5000个特征值时准确率为：%.5f' % accuracy_score(y_test, prediction))
+    knn.fit(x_train_pca4, y_train)
+    prediction = knn.predict(x_test_pca4)
+    print('保留10000个特征值时准确率为：%.5f' % accuracy_score(y_test, prediction))
+
+
+def task3():
+    imgProcess = ImgProcess('./dataset/')
+    x_train, y_train, x_test, y_test = imgProcess.run()
+    FDA = Fisher()
+    pca = PCA(n_components=50, whiten=True, random_state=0).fit(x_train)
+    x_train_pca = pca.transform(x_train)
+    x_test_pca = pca.transform(x_test)
+    FDA.initialization(x_train_pca, y_train)
+    result = FDA.predict(x_test_pca)
+    FDA.calculate_accuracy(result, y_test)
+
+
+if __name__ == '__main__':
+    task3()
